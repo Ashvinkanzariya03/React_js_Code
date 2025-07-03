61fdb389776788c2ede47826e5d22e7c31874776
Ashvin Kanzariya <ashvin@arhantsolutions.com>
2025-07-03 09:05:22 +0530

    Props

--- React-blog/src/App.jsx
+++ React-blog/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Multipleconditions from "./Multipleconditions";
+import PropsComponents from "./PropsComponents";
 
 function App() {
   const [display, setDisplay] = useState(true);
@@ -8,6 +9,7 @@ function App() {
       <button onClick={() => setDisplay(!display)}>Toggle</button>
       {display ? <h1>Ashvin</h1> : <h2>Kanzariya</h2>}
       <Multipleconditions/>
+      <PropsComponents name="PropsComponents" age={20} email="Ashvin@gamil.com"/>
     </>
   );
 }
